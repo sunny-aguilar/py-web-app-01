544f5df3edacb65cc85436daf31b5c2f290b8838
sunny-aguilar <sunny.aguilar@gmail.com>
2021-10-01 01:04:13 -0700

    large structure app 01

--- app/api/routes.py
+++ app/api/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 api_bp = Blueprint('api_bp', __name__, template_folder='templates')
 
-@api_bp.route('/api')
+@api_bp.route('/api', methods=['GET', 'POST'])
 def api():
   title = 'API Page'
   return render_template('api.html', title=title)
