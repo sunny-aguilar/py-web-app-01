ebda2ef65390978c811cd7213987ec9fc35f9d23
sunny-aguilar <sunny.aguilar@gmail.com>
2021-10-01 01:06:13 -0700

    large structure app 01

--- app/login/routes.py
+++ app/login/routes.py
@@ -8,4 +8,5 @@ def login():
   form = LoginForm()
   if form.validate_on_submit():
     flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+    return redirect('/')
   return render_template('login.html', title='Sign In', form=form)
