d39884886f5d773ae2279a60b0037b33a06bc152
sunny-aguilar <sunny.aguilar@gmail.com>
2021-10-01 01:04:46 -0700

    large structure app 01

--- app/login/routes.py
+++ app/login/routes.py
@@ -3,7 +3,7 @@ from app.forms import LoginForm
 
 login_bp = Blueprint('login_bp', __name__, template_folder='templates')
 
-@login_bp.route('/login')
+@login_bp.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
   return render_template('login.html', title='Sign In', form=form)
