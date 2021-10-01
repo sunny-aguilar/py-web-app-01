22e3189abf0b294a0aaafe371459686daf5c07a0
sunny-aguilar <sunny.aguilar@gmail.com>
2021-10-01 01:05:55 -0700

    large structure app 01

--- app/login/routes.py
+++ app/login/routes.py
@@ -6,4 +6,6 @@ login_bp = Blueprint('login_bp', __name__, template_folder='templates')
 @login_bp.route('/login', methods=['GET', 'POST'])
 def login():
   form = LoginForm()
+  if form.validate_on_submit():
+    flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
   return render_template('login.html', title='Sign In', form=form)
