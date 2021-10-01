0533e50affd109be5970ac0cbae6deaedaf9e696
sunny-aguilar <sunny.aguilar@gmail.com>
2021-09-30 22:15:53 -0700

    large structure app 01

--- app/index/routes.py
+++ app/index/routes.py
@@ -6,6 +6,8 @@ index_bp = Blueprint('index_bp', __name__, template_folder='templates')
 @index_bp.route('/index')
 def index():
   """ 2nd decorators adds another route to same function """
+  user = {'username': 'Sandro', }
+  posts = []
   msg = 'Index Page'
   title = 'Index Page'
   return render_template('index.html', msg=msg, title=title)
