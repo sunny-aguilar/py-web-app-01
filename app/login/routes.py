ee14febc9654cec0a4ac7a5f0f39ea43bd67c24e
sunny-aguilar <sunny.aguilar@gmail.com>
2021-10-01 00:31:17 -0700

    large structure app 01

--- app/login/routes.py
+++ app/login/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template
+from flask import Blueprint, render_template
 from app import app
 from app.forms import LoginForm
 
+login_bp = Blueprint('login_bp', __name__, template_folder='templates')
 
-@app.route('/login')
+@login_bp.route('/login')
 def login():
   form = LoginForm()
